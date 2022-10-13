a2a125df0266cb70809f5bbb177877d40363c300
Sergej Müller <sjmller@Mac-mini-von-Sergej.local>
2022-10-13 16:23:03 +0200

    Update options

--- lua/editor/options.lua
+++ lua/editor/options.lua
@@ -9,16 +9,24 @@ opt.relativenumber = true
 opt.termguicolors = true
 opt.background = "dark"
 
+opt.showcmd = true
+opt.cmdheight = 0
+opt.title = true
+
 -- Replace tabs with appropriate number of spaces
 opt.expandtab = true
 opt.tabstop = 2
 opt.shiftwidth = 2
 
-opt.autoindent = true -- Copy indent from current line when starting a new line
+-- Indentation rules
+opt.autoindent = true
+opt.smartindent = true
 
 opt.scrolloff = 5 -- Minimal number of screen lines to keep above and below the cursor
 opt.autoindent = true
+
 opt.wrap = true -- Lines longer than current window width will wrap
+opt.breakindent = true  -- Every wrapped line will continue visually indented
 
 opt.visualbell = true -- Use viusal bell instead of beeping
 
@@ -27,18 +35,8 @@ opt.foldmethod = "indent"
 opt.foldlevel = 99
 opt.foldenable = true
 
--- Use cache directory for backup and undo history
-local backup_dir = vim.fn.stdpath("cache") .. "/backup"
-local resp = vim.fn.mkdir(backup_dir, "p") -- create directory
-if resp == 1 then
-  opt.backupdir = backup_dir
-  opt.directory = backup_dir
-end
-
-local undo_dir = vim.fn.stdpath("cache") .. "/undo"
-resp = vim.fn.mkdir(undo_dir, "p")
-local has_persist = vim.fn.has("persistent_undo")
-if resp == 1 and has_persist == 1 then
-  opt.undofile = true
-  opt.undodir = undo_dir
-end
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = "set nopaste"
+})
