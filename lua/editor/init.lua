3de57cd7425ed12471cd4dad0431c8d1ed1edb18
Sergej Müller <sjmller@Mac-mini-von-Sergej.local>
2022-10-13 17:39:02 +0200

    Add Packer

--- lua/editor/init.lua
+++ lua/editor/init.lua
@@ -10,11 +10,15 @@ nvim.setup = function()
     "editor.autocmd",
   }) do
     local ok, err = pcall(require, module_name)
-    if not ok then 
-      print('Module: ' .. module_name .. ' not found!')
-      return
+    if not ok then
+      local msg = "calling module: " .. module_name .. " fail: " .. err
+      utils.errorL(msg)
     end
   end
+  -- since we have packer compiled, we don't need to load this immediately
+  vim.defer_fn(function()
+    require("plugins").init()
+  end, 0)
 end
 
 return nvim
--- lua/editor/utils.lua
+++ lua/editor/utils.lua
@@ -1,5 +1,23 @@
 local M = {}
 
+-- errorL notify a message in error level
+-- @msg: logging message
+-- @title: the logging title
+M.errorL = function(msg, title)
+  vim.notify(msg, vim.log.levels.ERROR, {
+    title = title,
+  })
+end
+
+-- infoL notify a message in info level
+-- @msg: logging message
+-- @title: the logging title
+M.infoL = function(msg, title)
+  vim.notify(msg, vim.log.levels.INFO, {
+    title = title,
+  })
+end
+
 -- map create a new mapping
 -- @param mode specify vim mode
 -- @param lhs specify the new keymap
