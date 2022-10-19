cc52a62648a6ee7097ad3b7e198afcde756212eb
Sergej Müller <sjmller@Mac-mini-von-Sergej.fritz.box>
2022-10-19 23:48:49 +0200

    Add default telescope mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,7 +12,7 @@ nmap("<", "<<")
 nmap(">", ">>")
 
 -- save quickly
-nmap("<space>w", ":w<CR>")
+nmap("<space>w", "<cmd>w<cr>")
 
 -- move around the window
 nmap("<space>k", "<C-w>k")
@@ -25,67 +25,14 @@ map("n", "<BS>", [["_X]], { noremap = false })
 map("v", "<BS>", [["_d]], { noremap = false })
 
 -- deactivate highliting after search
-nmap("<ESC>", ":nohlsearch<CR>")
+nmap("<ESC>", "<cmd>nohlsearch<cr>")
 
 -- nvim-tree
-nmap("<space>t", ":NvimTreeToggle<CR>")
+nmap("<space>t", ":NvimTreeToggle<cr>")
 
 -- telescope
-nmap("<leader>ff", function()
-  require("telescope.builtin").find_files(require("telescope.themes").get_dropdown())
-end)
-
-nmap("<leader>fo", ":Telescope oldfiles<CR>")
-
-nmap("<leader>s", function()
-  local pickers = require("telescope.pickers")
-  local finders = require("telescope.finders")
-  local conf = require("telescope.config").values
-  local actions = require("telescope.actions")
-  local action_state = require("telescope.actions.state")
-  local builtin = require("telescope.builtin")
-  local opts = {
-    sorting_strategy = "ascending",
-    results_title = false,
-    layout_strategy = "center",
-    layout_config = {
-      width = 50,
-      height = 9,
-    },
-    borderchars = {
-      prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
-      results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
-      preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    },
-  }
-
-  pickers
-      .new(opts, {
-        prompt_title = "Search",
-        finder = finders.new_table({
-          results = { "live grep", "buffer symbols", "workspace symbols" },
-        }),
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr, _)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-
-            local response = {
-              ["live grep"] = "live_grep",
-              ["buffer symbols"] = "lsp_document_symbols",
-              ["workspace symbols"] = "lsp_workspace_symbols",
-            }
-
-            local func = response[selection[1]]
-            if func == nil then
-              return
-            end
-
-            builtin[func](require("telescope.themes").get_ivy())
-          end)
-          return true
-        end,
-      })
-      :find()
-end)
+nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+nmap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+nmap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+nmap("<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+nmap("<leader>fo", "<cmd>Telescope oldfiles<cr>")
