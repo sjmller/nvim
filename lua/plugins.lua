fb2610c6a4548054cf038330bf52e51a8d519cae
Sergej Müller <sjmller@Mac-mini-von-Sergej.fritz.box>
2022-10-19 23:35:58 +0200

    Use suggested telescope update branch

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,7 +21,7 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup({ function(use)
   use 'wbthomason/packer.nvim'
-  use 'nvim-telescope/telescope.nvim'
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x' }
   use 'nvim-lua/plenary.nvim'
   use "kyazdani42/nvim-web-devicons"
   use 'nvim-tree/nvim-tree.lua'
